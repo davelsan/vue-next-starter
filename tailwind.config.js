9ff08f6c2f42ba5d1b9c38091427bfde2baf2fd8
David Velasco <davvelsan@gmail.com>
2020-05-01 17:09:20 +0200

    use tailwindcss built-in purge, update deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    mode: 'all',
+    content: [
+      './src/**/*.html',
+      './src/**/*.vue',
+      './src/**/*.jsx',
+    ],
+  },
   theme: {},
   variants: {},
   plugins: [],
