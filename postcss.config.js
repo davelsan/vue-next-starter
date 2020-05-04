a46061b70e116255d1947dcbb7d9b3217324a2a9
David Velasco <davvelsan@gmail.com>
2020-05-04 14:27:20 +0200

    use tailwind's built-in purge

--- postcss.config.js
+++ postcss.config.js
@@ -1,21 +1,6 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-
-  content: [
-    './src/**/*.html',
-    './src/**/*.js',
-    './src/**/*.ts',
-    './src/**/*.vue',
-  ],
-
-  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-});
-
 module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
-    ...process.env.NODE_ENV === 'production'
-      ? [purgecss]
-      : []
   ]
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      './src/**/*.html',
+      './src/**/*.ts',
+      './src/**/*.vue',
+    ],
+  },
   theme: {},
   variants: {},
   plugins: [],
